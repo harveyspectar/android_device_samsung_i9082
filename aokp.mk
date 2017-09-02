9afe07580db8172fd42570d4f36b426f7d9a4a47
harveyspectar <ultimatepravin72@gmail.com>
2017-09-02 16:33:26 +0530

    i9082: AOKP

--- aokp.mk
+++ aokp.mk
@@ -5,10 +5,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/samsung/i9082/device_i9082.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_i9082
+PRODUCT_NAME := aokp_i9082
 PRODUCT_DEVICE := i9082
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
